59a1d716b33f6eb8856d8a539a6bd1e7f974affe
danielsmota <danielmota@me.com>
2020-05-21 14:16:23 -0300

    updates

---
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MyAmazingComponent() {
+  return (
+    <div>
+      <h1 className="section-title section-title--blue">This Is My Amazing React Component</h1>
+      <p className="wrapper">React is great, the sky is blue, grass is green.</p>
+    </div>
+  )
+}
+
+export default MyAmazingComponent
